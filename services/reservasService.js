92792b019e3db96ff1f5b21fa6b00bb1d924d186
Vinicius Menezes <viniciusmenezes@Viniciuss-MacBook-Pro.local>
2019-03-28 15:27:56 -0300

    getall and getOne is created

--- services/reservasService.js
+++ services/reservasService.js
@@ -1,5 +1,24 @@
-const getAllReserva = (req, res) => { };
-const getOneReserva = (req, res) => { };
+const Reservas = require("../utils/DB");
+
+const getAllReserva = async (req, res) => {
+  try {
+    let result = await Reservas.findAll();
+    res.send(result);
+  } catch (e) {
+    res.status(500);
+    res.send(e);
+  }
+};
+
+const getOneReserva = async (req, res) => {
+  try {
+    let result = await Reservas.findById(req.params.id);
+    res.send(result);
+  } catch (e) {
+    res.status(500);
+    res.send(e);
+  }
+};
 const postReserva = (req, res) => { };
 const putReserva = (req, res) => { };
 const deleteReserva = (req, res) => { };
--- utils/DB.js
+++ utils/DB.js
@@ -1,8 +1,35 @@
 const uri = require("../config/uri");
-const mongo = require("mongodb").MongoClient;
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
+const client = new MongoClient(uri.mlab, { useNewUrlParser: true });
+const dbName = "backend-assistant";
+const collection = 'Reservas';
+let db;
 
-mongo.connect(uri.mlab, { useNewUrlParser: true }, (err, client) => {
-  if (err) return console.log(err);
-  db = client.db("backend-assistant");
-});
+const connect = async () => {
+  await client.connect();
+  db = client.db(dbName);
+}
 
+const query = async (coll, predicate) => {
+  try {
+    await connect();
+    let results = db.collection(coll).find(predicate).toArray();
+    return results;
+  } catch (e) {
+    console.error(e.stack);
+    throw (e.stack);
+  }
+  client.close();
+}
+
+const findAll = () => {
+  return query(collection, {});
+}
+
+const findById = (id) => {
+  return query(collection, { _id: ObjectId(id) });
+}
+
+exports.findAll = findAll;
+exports.findById = findById;
