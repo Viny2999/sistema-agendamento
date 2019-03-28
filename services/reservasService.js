ac89e41e20d05e612a8d2429d16a9182d1124dfd
Vinicius Menezes <viniciusmenezes@Viniciuss-MacBook-Pro.local>
2019-03-28 16:23:14 -0300

    create put and delete

--- services/reservasService.js
+++ services/reservasService.js
@@ -19,9 +19,36 @@ const getOneReserva = async (req, res) => {
     res.send(e);
   }
 };
-const postReserva = (req, res) => { };
-const putReserva = (req, res) => { };
-const deleteReserva = (req, res) => { };
+
+const postReserva = async (req, res) => {
+  try {
+    let result = await Reservas.insertOne(req.body);
+    res.send(result);
+  } catch (e) {
+    res.status(500);
+    res.send(e);
+  }
+};
+
+const putReserva = async (req, res) => {
+  try {
+    let result = await Reservas.updateOne(req.params.id, req.body);
+    res.send(result);
+  } catch (e) {
+    res.status(500);
+    res.send(e);
+  }
+};
+
+const deleteReserva = async (req, res) => {
+  try {
+    let result = await Reservas.deleteOne(req.params.id);
+    res.send(result);
+  } catch (e) {
+    res.status(500);
+    res.send(e);
+  }
+};
 
 exports.getAllReserva = getAllReserva;
 exports.getOneReserva = getOneReserva;
--- utils/DB.js
+++ utils/DB.js
@@ -11,7 +11,7 @@ const connect = async () => {
   db = client.db(dbName);
 }
 
-const query = async (coll, predicate) => {
+const find = async (coll, predicate) => {
   try {
     await connect();
     let results = db.collection(coll).find(predicate).toArray();
@@ -24,12 +24,50 @@ const query = async (coll, predicate) => {
 }
 
 const findAll = () => {
-  return query(collection, {});
+  return find(collection, {});
 }
 
 const findById = (id) => {
-  return query(collection, { _id: ObjectId(id) });
+  return find(collection, { _id: ObjectId(id) });
 }
 
+const insertOne = async (obj) => {
+  try {
+    await connect();
+    db.collection(collection).insertOne(obj);
+    return obj;
+  } catch (e) {
+    console.error(e.stack);
+    throw (e.stack);
+  }
+  client.close();
+}
+
+const updateOne = async (id, obj) => {
+  try {
+    await connect();
+    db.collection(collection).updateOne({ _id: ObjectId(id) }, { $set: obj });
+    return obj;
+  } catch (e) {
+    console.error(e.stack);
+    throw (e.stack);
+  }
+  client.close();
+}
+
+const deleteOne = async (id) => {
+  try {
+    await connect();
+    db.collection(collection).deleteOne({ _id: ObjectId(id) });
+    return { status: "Success" };
+  } catch (e) {
+    console.error(e.stack);
+    throw (e.stack);
+  }
+  client.close();
+}
 exports.findAll = findAll;
 exports.findById = findById;
+exports.insertOne = insertOne;
+exports.updateOne = updateOne;
+exports.deleteOne = deleteOne;
